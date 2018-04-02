87ab2765c82a7ae6b5d58af7c3a878a440ef1259
Herz3h <cresuso@gmail.com>
2018-04-02 16:11:29 +0200

    Install script updated

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 
-copy .vimrc ~/
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     cp .vimrc ~/;;
+    Darwin*)    cp .vimrc ~/;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     copy .vimrc ~/;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
