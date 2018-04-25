8f935482ad8ebedbd9d481a6efd81cf10fdfb9a5
Ahmed EL MODEN <cresuso@gmail.com>
2018-04-25 08:41:43 +0200

    Adding tmux copy-mode binding, updated install script

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     ln -rsf .vimrc ~/.vimrc; ln -rsf .tmux.conf ~/.tmux.conf ;;
-    Darwin*)    ln -rsf .vimrc ~/.vimrc; ln -rsf .tmux.conf ~/.tmux.conf ;;
+    Linux*)     ln -sf ~/.vim/.vimrc ~/.vimrc; ln -sf ~/.vim/.tmux.conf ~/.tmux.conf ;;
+    Darwin*)    ln -sf ~/.vim/.vimrc ~/.vimrc; ln -sf ~/.vim/.tmux.conf ~/.tmux.conf ;;
     CYGWIN*)    machine=Cygwin;;
     MINGW*)     mklink .vimrc ~/;;
     *)          machine="UNKNOWN:${unameOut}"
